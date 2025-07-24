ace2ba96f0db460b8883dedb78668ba35cfbf2f7
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-07-24 12:22:19 +0000

    function contoh list

---
+++ Collections.kt
@@ -0,0 +1,11 @@
+package com.example.test
+
+fun ContohList() {
+    println("=== List ===")
+
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
