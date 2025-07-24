7c9ef25060e8314dbfba8f126f799f89cbc3029e
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-07-24 12:19:05 +0000

    add main function

--- Class.kt
+++ Class.kt
@@ -3,3 +3,11 @@ package com.example.test
 class Motor()
 
 class Contact(val id: Int, var email: String)
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+    println(contact.email)
+
+    // commit: update email
+    contact.email = "jane@gmail.com"
+    println(contact.email)
+}
